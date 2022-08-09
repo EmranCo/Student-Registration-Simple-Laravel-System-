6a72453042e20e3dc388b604590b80654846ac76
emranco <emranhadad770774255770774255@gmail.com>
2022-08-10 01:03:47 +0300

    change success message of student registration

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\StudentController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\URL;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+if (env('APP_ENV') === 'production') {
+    URL::forceSchema('https');
+}
 
 Route::get('/', [StudentController::class,'index'])->name('index');
 Route::post('/register_student', [StudentController::class,'store'])->name('store');
